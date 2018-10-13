fed2e79c4aed7e9ceb288f46c2450d2f4ea6ea3e
erald <erald@EDISTRIBUTION.local>
2018-10-13 15:26:51 +0200

    tab remove & tab selection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,16 +10,22 @@ import { TasksService } from '../services/tasks/tasks.service';
 export class TasksComponent implements OnInit {
   currentUser;
   tabs = [];
+  tabSelectedIndex = 0;
 
   constructor(private afAuth: AngularFireAuth, private tasksService: TasksService) {
     tasksService.tasksEdited$.subscribe(
       task => {
-        if (!this.tabs[this.currentUser]) {
-          this.tabs[this.currentUser] = [];
+        if (!this.tabs[this.currentUser]) { // si le tableau de taches ouvertes (onglet) n'existe pas pour cet utilisateur
+          this.tabs[this.currentUser] = []; // on le crée
         }
-        this.tabs[this.currentUser].push(task);
-        console.log(task[2]);
-        console.log(this.tabs);
+        let indexTab = this.findTaskinTabs(task[1]);
+        if (indexTab.length < 1) {// si l'onglet n'est pas deja ouvert
+          this.tabs[this.currentUser].push(task); // puis on y ajoute la tache sélectionnée (clic sur btn édit dans liste de tache)
+          indexTab = this.findTaskinTabs(task[1]);
+          this.tabSelectedIndex = indexTab[0] + 2;
+        } else {
+          this.tabSelectedIndex = indexTab[0] + 2;
+        }
     });
    }
 
@@ -32,6 +38,23 @@ export class TasksComponent implements OnInit {
 
   addTab(data) {
     console.log(data[0]);
+  }
+
+  closeTab (task: string) {
+    const indexTab = this.findTaskinTabs(task);
+    for (let i = 0; i < indexTab.length; i++) {
+      this.tabs[this.currentUser].splice(indexTab[i], 1);
+    }
+  }
+
+  findTaskinTabs (taskName): number[] {
+    const indexTab = [];
+    for (let i = 0; i < this.tabs[this.currentUser].length ; i++) {
+      if (this.tabs[this.currentUser][i].indexOf(taskName) > -1) {
+        indexTab.push(i);
+      }
+    }
+    return indexTab;
   }
 
 }
